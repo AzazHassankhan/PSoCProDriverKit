546d779a650637c2d948468ce6dec06f51441726
Azaz Hassan Khan <92155897+AzazHassankhan@users.noreply.github.com>
2023-10-15 15:42:13 +0500

    Update main.c

--- UART_Communication/main.c
+++ UART_Communication/main.c
@@ -1,4 +1,4 @@
-/* ========================================
+/*========================================
  *
  *Copyright YOUR COMPANY, THE YEAR
  *All Rights Reserved
@@ -13,16 +13,17 @@
 #include "UartFuncFile.h"
 #include "stdio.h"
 
-
 int main(void)
 {
-	CyGlobalIntEnable; /*Enable global interrupts. */
-	
-	
-	UART_1_Start();
+	  CyGlobalIntEnable; /*Enable global interrupts. */
+
+	  UART_1_Start();
+	  /*Enable global interrupts. 
     Uart_String_Func();
     Uart_Int_Func();
-	
+	  while(1)
+    {Uart_get_Func();
+    CyDelay(100);} */
 }
 
 /*[] END OF FILE */
